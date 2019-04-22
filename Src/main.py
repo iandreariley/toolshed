59f445aa31d77b40345eb0aee6f5e64583d52ef0
Ian Riley <iandreariley@gmail.com>
2019-04-21 19:11:46 -0700

    Make this publishable. Sort of.

--- Src/main.py
+++ Src/main.py
@@ -5,17 +5,17 @@ import sys
 from cli import command_dispatcher
 
 
-logger = logging.getLogger("project")
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+def run():
+    logger = logging.getLogger("project")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-parser = argparse.ArgumentParser("A place to save your dev tools so you don't have to make them all over again.")
-actions = command_dispatcher.registry.keys()
-parser.add_argument('action', help='Choose from these actions', choices=list(actions))
-known_args, unknown_args = parser.parse_known_args()
-command_dispatcher(sys.argv[1:])
+    parser = argparse.ArgumentParser("A place to save your dev tools so you don't have to make them all over again.")
+    actions = command_dispatcher.registry.keys()
+    parser.add_argument('action', help='Choose from these actions', choices=list(actions))
+    command_dispatcher(sys.argv[1:])
 
