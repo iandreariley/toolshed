2f9f4f186e39642f2602e5b67aac995ac88460ec
Ian Riley <iandreariley@gmail.com>
2019-04-21 18:03:38 -0700

    Make slightly more enlightening description message.

--- Src/main.py
+++ Src/main.py
@@ -13,9 +13,9 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-parser = argparse.ArgumentParser("A cool place to store your tools out of the hot sun.")
+parser = argparse.ArgumentParser("A place to save your dev tools so you don't have to make them all over again.")
 actions = command_dispatcher.registry.keys()
-parser.add_argument('action', help=pprint.pformat(actions), choices=actions)
+parser.add_argument('action', help='Choose from these actions', choices=list(actions))
 known_args, unknown_args = parser.parse_known_args()
 command_dispatcher(sys.argv[1:])
 
