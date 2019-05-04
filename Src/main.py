7c87d2b1e5ff61a633e4d347d9c1b16a202bee15
Ian Riley <iandreariley@gmail.com>
2019-05-04 14:53:44 -0700

    Create HOME directory by default.

--- Src/main.py
+++ Src/main.py
@@ -1,7 +1,9 @@
 import argparse
 import logging
+import os
 import sys
 
+import shed
 from cli import command_dispatcher
 
 
@@ -13,6 +15,9 @@ def run():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
+    # Create home directory if it doesn't exist
+    os.makedirs(shed.HOME, exist_ok=True)
 
     parser = argparse.ArgumentParser("A place to save your dev tools so you don't have to make them all over again.")
     actions = command_dispatcher.registry.keys()
